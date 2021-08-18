a074dad99c95cf15b83cdd020c09a0bc701e6a9f
Jacek Roszkowiak <jacekroszkowiakdev@gmail.com>
2021-08-18 18:15:49 +0200

    typescript issues overcomed, routes modules now loading correctly

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import App from "./App";
 import { Home } from "./components/Home";
 import { QuestionCard } from "./components/QuestionCard";
 import { Results } from "./components/Results";
+
+interface RouteProps {
+    path?: string;
+}
 
 export const Root = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/quiz/" component={QuestionCard} />
-                <Route path="/results/" component={Results} />
+                <Route<RouteProps> path="/" exact component={Home} />
+                <Route<RouteProps> path="/quiz/" component={QuestionCard} />
+                <Route<RouteProps> path="/results/" component={Results} />
             </Switch>
         </BrowserRouter>
     );
