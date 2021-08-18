b4b448e01e5c5bf5e00532718bf34a099be78c99
Jacek Roszkowiak <jacekroszkowiakdev@gmail.com>
2021-08-18 02:25:13 +0200

    additional material UI component added, issue with import solved

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,15 @@
-import { Button } from "./../../UI/Button";
+import { ContainedButton } from "./../../UI/Button";
 
 export const Home = () => {
     return (
-        <div>
+        <div
+            className="Home"
+            style={{ border: "1px solid", borderRadius: "15px", width: "60vw" }}
+        >
             <h1>Welcome to the Trivia Challenge</h1>
             <p>You will be presented with 10 True or False questions.</p>
             <p>Can you score 100%?</p>
-            <Button />
+            <ContainedButton />
         </div>
     );
 };
